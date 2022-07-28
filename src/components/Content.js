db68824d75922f1c5f6c2ab51baba9d4106c0ae9
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-07-28 18:20:35 +0300

    header

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Content() {
+  return (
+      <div>
+          Ben Header içerisinde Content
+      </div>
+  )
+}
+
+export default Content
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Content from './Content'
+
+function Header() {
+  return (
+      <nav>
+          Ben bir Header
+          <Content/>
+      </nav>
+  )
+}
+
+export default Header
