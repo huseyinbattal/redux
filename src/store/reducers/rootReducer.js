85d9816f3529beb2be3b4c5edab05e590a6338a2
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-07-29 16:56:15 +0300

    Update rootReducer.js

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,19 +1,34 @@
+import {combineReducers} from "redux"
+
+
 const initialState = {
   count: 0,
 };
 
-function rootReducer(state = initialState, action) {
-  
-switch (action.type) {
-  case "INCREMENT":
-    return {count:state.count+action.payload}
-  case "DECREMENT":
-    return {count:state.count-action.payload}
+const initialUserState = {
+  userName: 0,
+};
 
-  default:
-    return state
+function userReducer(state=initialUserState,action) {
+  switch (action.type) {
+    case "SET_USERNAME":
+      return { userName:action.payload};
+  
+    default:
+      return state;
+  }
 }
 
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + action.payload };
+    case "DECREMENT":
+      return { count: state.count - action.payload };
+
+    default:
+      return state;
+  }
 }
 
 export default rootReducer;
