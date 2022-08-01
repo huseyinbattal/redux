8c85873c9ac22affffad2efe9f78bdc2fc94cc49
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-08-01 06:08:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Header from "./components/Header";
-import {SET_USERNAME} from "./store/actions/userActions"
+import { SET_USERNAME } from "./store/actions/userActions";
+import { setUserName } from "./store/actions/userActions";
 
 function App() {
-
-  const [userName, setUserName] = useState("emir")
+  const [userName] = useState("emir");
   const dispatch = useDispatch();
- // const count=useSelector((state)=>state.count)
+  // const count=useSelector((state)=>state.count)
   return (
     <div className="App">
-      <button onClick={() => {
-        dispatch(setUserName("hüseyin"))
-      }}>Change Name</button>
+      <button
+        onClick={() => {
+          dispatch(setUserName("hüseyin"));
+        }}
+      >
+        Change Name
+      </button>
       <header className="App-header">
         Sayı:
         <Header
